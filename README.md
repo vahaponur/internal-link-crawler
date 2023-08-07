771a81b7f07654463e0a2643f1deffb6246efecb
Vahap <vahap@192.168.1.52>
2023-08-07 20:51:04 +0300

    finish

--- README.md
+++ README.md
@@ -1,37 +1,3 @@
 # Internal Link Crawler
 
 Generates an "internal links" report for any website on the internet by crawling each page of the site.
-
-## Installation
-
-Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
-
-```bash
-pip install foobar
-```
-
-## Usage
-
-```python
-import foobar
-
-# returns 'words'
-foobar.pluralize('word')
-
-# returns 'geese'
-foobar.pluralize('goose')
-
-# returns 'phenomenon'
-foobar.singularize('phenomena')
-```
-
-## Contributing
-
-Pull requests are welcome. For major changes, please open an issue first
-to discuss what you would like to change.
-
-Please make sure to update tests as appropriate.
-
-## License
-
-[MIT](https://choosealicense.com/licenses/mit/)
--- report.js
+++ report.js
@@ -1,13 +1,45 @@
-const printReport=(pages)=>{
-    const sorted=quickSort(pages)
+const printReport = (pages) => {
+    const sorted = quickSortedPages(pages);
 
-    for (const key in sorted) {
-        console.log(`Found ${sorted[key]} internal links to ${key}`)
+    sorted.forEach((element) => {
+        console.log(`Found ${element[1]} internal links to ${element[0]}`);
+    });
+};
+
+const quickSortedPages = (pages) => {
+    const arr = Object.entries(pages);
+    quickSort(arr, 0, arr.length - 1);
+    return arr;
+};
+
+const quickSort = (kvArr, start, end) => {
+    if (end <= start) return;
+    let pivot = partition(kvArr, start, end);
+    quickSort(kvArr, start, pivot - 1);
+    quickSort(kvArr, pivot + 1, end);
+};
+
+const partition = (kvArr, start, end) => {
+    let pivot = kvArr[end][1];
+    let pivotEntry = kvArr[end];
+    let i = start - 1;
+    let temp = 0;
+    for (let j = start; j <= end - 1; j++) {
+        if (kvArr[j][1] < pivot) {
+            i++;
+            temp = kvArr[i];
+            kvArr[i] = kvArr[j];
+            kvArr[j] = temp;
+        }
     }
-}
-const quickSort=(pages)=>{
-    return pages
-}
+
+    i++;
+    temp = kvArr[i];
+    kvArr[i] = pivotEntry;
+    kvArr[end] = temp;
+
+    return i;
+};
 
 module.exports={
     printReport
